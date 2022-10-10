3bfdf92310876278088ca906f40b83010e1432df
tuandang <tuandangvan1699@gmail.com>
2022-10-10 13:02:54 +0700

    update

--- src/js/diet.js
+++ src/js/diet.js
@@ -1,3 +1,7 @@
 import "../css/diet.css";
+import "../component/auth.css";
+import "../component/footer.css";
+import "../component/sidebar.css";
+import "../component/header.css";
 
 import "../js/modal.js";
