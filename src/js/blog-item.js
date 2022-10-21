78d173bc088d303b4bfc7428b5b17065e09976ad
tuandang <tuandangvan1699@gmail.com>
2022-10-21 11:13:54 +0700

    update

--- src/js/blog-item.js
+++ src/js/blog-item.js
@@ -1,7 +1,25 @@
-import "../css/blog-item.css";
 import "../component/auth.css";
 import "../component/footer.css";
 import "../component/sidebar.css";
 import "../component/header.css";
+import "../css/blog-item.css";
 
 import "../js/modal.js";
+
+import { blogList } from "./db";
+
+import $ from "jquery";
+import _ from "lodash";
+
+$(function () {
+  const url = new URL(location.href);
+  const id = Number(url.searchParams.get("id"));
+  const blog = _.find(blogList, { id });
+
+  const blogTemplate = $("#blogItemTemplate").html();
+  const blogTemp = _.template(blogTemplate);
+
+  const dom = $(blogTemp(blog));
+
+  $(".blog-container").append(dom);
+});
--- src/js/blog.js
+++ src/js/blog.js
@@ -3,5 +3,25 @@ import "../component/auth.css";
 import "../component/footer.css";
 import "../component/sidebar.css";
 import "../component/header.css";
+import $ from "jquery";
+import _ from "lodash";
+import { blogList } from "./db";
+import { addToFav } from "./ulist";
 
 import "../js/modal.js";
+
+$(function () {
+  const blogTemplate = $("#blogTemplate").html();
+  const blogTemp = _.template(blogTemplate);
+
+  $(".blog-list").append(
+    _.map(blogList, (blog) => {
+      const dom = $(blogTemp(blog));
+
+      dom.find(".add-favourite").on("click", blog, addToFav);
+
+      return dom;
+    })
+  );
+});
+
